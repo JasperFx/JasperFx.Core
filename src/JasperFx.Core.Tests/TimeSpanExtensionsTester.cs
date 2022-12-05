34a684a0483ca8936442a4cb7d4d7e01d256e177
Jeremy D. Miller <Jeremy.Miller@medeanalytics.com>
2022-12-04 19:17:48 -0600

    added the TimeSpan extensions

---
+++ src/JasperFx.Core.Tests/TimeSpanExtensionsTester.cs
@@ -0,0 +1,48 @@
+using Shouldly;
+
+namespace JasperFx.Core.Tests
+{
+    
+    public class TimeSpanExtensionsTester
+    {
+        [Fact]
+        public void to_time_from_int()
+        {
+            700.ToTime().ShouldBe(new TimeSpan(7, 0, 0));
+            1700.ToTime().ShouldBe(new TimeSpan(17, 0, 0));
+            1850.ToTime().ShouldBe(new TimeSpan(18, 50, 0));
+        }
+
+        [Fact]
+        public void to_time_from_string()
+        {
+            "0700".ToTime().ShouldBe(new TimeSpan(7, 0, 0));
+            "1700".ToTime().ShouldBe(new TimeSpan(17, 0, 0));
+            "1850".ToTime().ShouldBe(new TimeSpan(18, 50, 0));
+        }
+
+        [Fact]
+        public void Minutes()
+        {
+            5.Minutes().ShouldBe(new TimeSpan(0, 5, 0));
+        }
+
+        [Fact]
+        public void hours()
+        {
+            6.Hours().ShouldBe(new TimeSpan(6, 0, 0));
+        }
+
+        [Fact]
+        public void days()
+        {
+            2.Days().ShouldBe(new TimeSpan(2, 0, 0, 0));
+        }
+
+        [Fact]
+        public void seconds()
+        {
+            8.Seconds().ShouldBe(new TimeSpan(0, 0, 8));
+        }
+    }
+}
