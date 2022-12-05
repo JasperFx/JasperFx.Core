7cc51a54079e236051ed6f31c7375f509937a5b7
Jeremy D. Miller <Jeremy.Miller@medeanalytics.com>
2022-12-05 08:43:15 -0600

    added disposable extensions

---
+++ src/JasperFx.Core.Tests/DisposableExtensionsTests.cs
@@ -0,0 +1,27 @@
+﻿using Shouldly;
+
+namespace JasperFx.Core.Tests
+{
+    public class DisposableExtensionsTests
+    {
+        [Fact]
+        public void swallow_exceptions()
+        {
+            var blowsUpDisposing = new BlowsUpDisposing();
+            blowsUpDisposing.SafeDispose();
+
+            blowsUpDisposing.DisposeWasCalled.ShouldBeTrue();
+        }
+
+        public class BlowsUpDisposing : IDisposable
+        {
+            public void Dispose()
+            {
+                DisposeWasCalled = true;
+                throw new Exception("You stink!");
+            }
+
+            public bool DisposeWasCalled { get; set; }
+        }
+    }
+}
