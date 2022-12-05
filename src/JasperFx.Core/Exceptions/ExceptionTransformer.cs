c66ac91f684b3a277a9a2e39cece0a9d882ce475
Jeremy D. Miller <Jeremy.Miller@medeanalytics.com>
2022-12-05 08:39:10 -0600

    imported ExceptionTransformer

---
+++ src/JasperFx.Core/Exceptions/ExceptionTransformer.cs
@@ -0,0 +1,15 @@
+﻿namespace JasperFx.Core.Exceptions;
+
+/// <summary>
+/// Helper to transform exceptions into hopefully more user friendly exceptions. Static wrapper around
+/// ExceptionTransforms
+/// </summary>
+public static class ExceptionTransformer
+{
+    public static readonly ExceptionTransforms Transforms = new ExceptionTransforms();
+    
+    public static void WrapAndThrow(Exception exception)
+    {
+        Transforms.TransformAndThrow(exception);
+    }
+}
