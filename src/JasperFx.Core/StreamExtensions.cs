9b2e0baa3191cbc6024aa4fd12a5da472244630c
Jeremy D. Miller <Jeremy.Miller@medeanalytics.com>
2023-05-01 07:35:58 -0500

    imported sequential guid generation, task timeout, Uri, stream utilities from Wolverine. Bumps to 1.4

--- src/JasperFx.Core/StreamExtensions.cs
+++ src/JasperFx.Core/StreamExtensions.cs
@@ -51,5 +51,55 @@
             await stream.CopyToAsync(content).ConfigureAwait(false);
             return content.ToArray();
         }
+        
+        /// <summary>
+        /// Read Byte array from stream
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public static async Task<byte[]> ReadBytesAsync(this Stream stream, long length)
+        {
+            var buffer = new byte[length];
+            var totalRead = 0;
+            int current;
+            do
+            {
+                current = await stream.ReadAsync(buffer, totalRead, buffer.Length - totalRead).ConfigureAwait(false);
+                totalRead += current;
+            } while (totalRead < length && current > 0);
+
+            return buffer;
+        }
+
+        /// <summary>
+        /// Tests a stream to see if it starts with an expected byte sequence
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <param name="expected"></param>
+        /// <returns></returns>
+        public static async Task<bool> ReadExpectedBufferAsync(this Stream stream, byte[] expected)
+        {
+            try
+            {
+                var bytes = await stream.ReadBytesAsync(expected.Length).ConfigureAwait(false);
+                return expected.SequenceEqual(bytes);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Write a byte array to a stream
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <param name="buffer"></param>
+        /// <returns></returns>
+        public static Task SendBufferAsync(this Stream stream, byte[] buffer)
+        {
+            return stream.WriteAsync(buffer, 0, buffer.Length);
+        }
     }
 }
