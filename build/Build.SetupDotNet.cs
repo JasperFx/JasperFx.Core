782088f3ba4f14266f9644f6943a70b681b3abed
JT <Hawxy@users.noreply.github.com>
2025-02-05 20:51:51 +0800

    Add Nuke.Build, remove net6/7 targets

---
+++ build/Build.SetupDotNet.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using Nuke.Common.CI.GitHubActions;
+using Nuke.Common.CI.GitHubActions.Configuration;
+using Nuke.Common.Execution;
+using Nuke.Common.Utilities;
+
+public class GitHubActionsSetupDotNetStep : GitHubActionsStep
+{
+    public string[] Versions { get; set; }
+
+    public override void Write(CustomFileWriter writer)
+    {
+        writer.WriteLine("- uses: actions/setup-dotnet@v4");
+
+        using (writer.Indent())
+        {
+            writer.WriteLine("with:");
+            using (writer.Indent())
+            {
+                writer.WriteLine("dotnet-version: |");
+                using (writer.Indent())
+                {
+                    foreach (var version in Versions)
+                    {
+                        writer.WriteLine(version);
+                    }
+                }
+            }
+        }
+    }
+}
+
+public class GithubActionsExtendedAttribute : GitHubActionsAttribute
+{
+    public string[] SetupDotNetVersions { get; set; } = [];
+    public GithubActionsExtendedAttribute(string name, GitHubActionsImage image, params GitHubActionsImage[] images) : base(name, image, images)
+    {
+    }
+
+    protected override GitHubActionsJob GetJobs(GitHubActionsImage image,
+        IReadOnlyCollection<ExecutableTarget> relevantTargets)
+    {
+        var job = base.GetJobs(image, relevantTargets);
+
+        var newSteps = new List<GitHubActionsStep>(job.Steps);
+        newSteps.Insert(0, new GitHubActionsSetupDotNetStep(){Versions = SetupDotNetVersions});
+
+        job.Steps = newSteps.ToArray();
+        
+        return job;
+    }
+}
---
+++ build/Build.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Linq;
+using Nuke.Common;
+using Nuke.Common.CI;
+using Nuke.Common.CI.GitHubActions;
+using Nuke.Common.Execution;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Utilities.Collections;
+using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+[GithubActionsExtended(
+    "Build & Test",
+    GitHubActionsImage.UbuntuLatest,
+    OnPushBranches = ["master"],
+    OnPullRequestBranches = ["master"],
+    InvokedTargets = [nameof(Test)],
+    SetupDotNetVersions = ["8.0", "9.0"])]
+[GithubActionsExtended(
+    "Manual Nuget Push",
+    GitHubActionsImage.UbuntuLatest,
+    On = [GitHubActionsTrigger.WorkflowDispatch],
+    InvokedTargets = [nameof(NugetPush)],
+    ImportSecrets = [nameof(NugetApiKey)],
+    SetupDotNetVersions = ["8.0", "9.0"])]
+class Build : NukeBuild
+{
+    /// Support plugins are available for:
+    ///   - JetBrains ReSharper        https://nuke.build/resharper
+    ///   - JetBrains Rider            https://nuke.build/rider
+    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
+    ///   - Microsoft VSCode           https://nuke.build/vscode
+
+    public static int Main () => Execute<Build>(x => x.Compile);
+    
+    AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
+
+    [Solution(GenerateProjects = true)] readonly Solution Solution;
+
+    Target Clean => _ => _
+        .Before(Restore)
+        .Executes(() =>
+        {
+            ArtifactsDirectory.CreateOrCleanDirectory();
+        });
+
+    Target Restore => _ => _
+        .Executes(() =>
+        {
+            DotNetRestore(s => s
+                .SetProjectFile(Solution));
+        });
+
+    Target Compile => _ => _
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+            DotNetBuild(s => s
+                .SetProjectFile(Solution)
+                .SetConfiguration("Release")
+                .EnableNoRestore());
+        });
+    
+    Target Test => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            DotNetTest(_ => _
+                .SetProjectFile(Solution.JasperFx_Core_Tests));
+        });
+
+    Target NugetPack => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            DotNetPack(_ => _
+                .SetProject(Solution.JasperFx_Core)
+                .SetConfiguration("Release")
+                .EnableContinuousIntegrationBuild()
+                .SetOutputDirectory(ArtifactsDirectory));
+        });
+
+    [Parameter("Nuget Api Key")] [Secret] readonly string NugetApiKey;
+
+    Target NugetPush => _ => _
+        .DependsOn(NugetPack)
+        .Requires(() => !string.IsNullOrEmpty(NugetApiKey))
+        .Executes(() =>
+        {
+            DotNetNuGetPush(_ => _
+                .SetSource("https://api.nuget.org/v3/index.json")
+                .SetTargetPath(ArtifactsDirectory / "*.nupkg")
+                .EnableSkipDuplicate()
+                .EnableNoSymbols()
+                .SetApiKey(NugetApiKey));
+        });
+
+}
